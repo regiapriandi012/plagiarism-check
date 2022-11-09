6ccec7afdeba1c421e48a589ef1183a020b50408
Regi Apriandi <regiapriandi012@gmail.com>
2022-11-09 18:17:24 +0700

    push algorithm

--- teset.py
+++ teset.py
@@ -1,14 +1,17 @@
 from googlesearch import search
 
-# to search
-query = '"Python has many libraries that really help developers in creating very extraordinary applications."'
+word = "jflkdsjf dsfkdsj flkdsjlkf dsjfklds jflkds jflkdsj flkdsj lkfjds fklds jkfdsjf dslkjflkds fkdsjlkf flkjdslkf dsflkds jfkdsfskdl."
 
-for j in search(query, tld="co.id", num=10, stop=10, pause=2):
-    print(j)
-
-#%%
-word = "lkjskdjsljf fdsjflkds f flkdsjf lkds. dlksjfdkls lkjfdsj flkdsj  lkfdsj fkdls"
-data = word.split(". ")
+data = word.replace("\n", " ").split(". ")
+hasil = []
 for i in data:
-    var = '"' + i + '"'
-    print(var)
+    query = '"' + i.strip().replace(".", "").replace('"', "'") + '"'
+    for j in search(query, tld="co.id", num=10, stop=10, pause=2):
+        hasil.append(j)
+
+count = len(data)
+count_hasil = len(hasil)
+hasil_persen = (count_hasil / count) * 100
+for i in range(len(hasil)):
+    print('https://www.google.com/search?q="{}"'.format(data[i]).replace(" ", "%20"))
+print(hasil_persen, "%")
