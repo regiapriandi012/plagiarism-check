cf523418e8c4224000df60337e689b761ef144e9
Regi Apriandi <regiapriandi012@gmail.com>
2022-11-10 01:08:16 +0700

    bismillah

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect, url_for, make_response
+from googlesearch import search
+import re
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        word = request.form['text']
+        with open('word.txt', 'w', encoding='utf-8') as f:
+            f.write(word)
+        return redirect(url_for('plagiarism')+"#hasil")
     return render_template("index.html")
+
+@app.route('/plagiarism', methods=['GET', 'POST'])
+def plagiarism():
+    link_output = []
+    hasil_plagiarism = []
+    hasil_link = []
+    hasil_persen = 0
+    f = open("word.txt", "r")
+    inputan_mentah = f.read()
+    inputan = inputan_mentah.replace("\n", " ").split(". ")
+    for i in range(len(inputan)):
+        query = '"' + inputan[i].strip().replace(".", "").replace('"', "'") + '"'
+        for j in range(len(list(search(query, tld="com", num=10, stop=10, pause=2)))):
+            if i != j:
+                continue
+            hasil_plagiarism.append(inputan[i])
+            hasil_link.append(list(search(query, tld="com", num=10, stop=10, pause=2))[j])
+    count = len(inputan)
+    count_hasil = len(hasil_link)
+    hasil_persen += (count_hasil / count) * 100
+    for i in range(len(hasil_link)):
+        link_output.append('https://www.google.com/search?q="{}"'.format(hasil_link[i]).replace(" ", "%20"))
+
+    return render_template("index.html", hasil_persen=hasil_persen, data=inputan, hasil_plagiarism=hasil_plagiarism, link_output=link_output)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
--- teset.py
+++ teset.py
@@ -1,17 +1,14 @@
 from googlesearch import search
-
-word = "jflkdsjf dsfkdsj flkdsjlkf dsjfklds jflkds jflkdsj flkdsj lkfjds fklds jkfdsjf dslkjflkds fkdsjlkf flkjdslkf dsflkds jfkdsfskdl."
-
-data = word.replace("\n", " ").split(". ")
-hasil = []
-for i in data:
-    query = '"' + i.strip().replace(".", "").replace('"', "'") + '"'
-    for j in search(query, tld="co.id", num=10, stop=10, pause=2):
-        hasil.append(j)
-
-count = len(data)
-count_hasil = len(hasil)
-hasil_persen = (count_hasil / count) * 100
-for i in range(len(hasil)):
-    print('https://www.google.com/search?q="{}"'.format(data[i]).replace(" ", "%20"))
-print(hasil_persen, "%")
+f = open("word.txt", "r")
+inputan_mentah = f.read()
+inputan = inputan_mentah.replace("\n", " ").split(". ")
+hasil_plagiarism = []
+link_output = []
+hasil_link = []
+for i in range(len(inputan)):
+    query = '"' + inputan[i].strip().replace(".", "").replace('"', "'") + '"'
+    for j in range(len(list(search(query, tld="co.in", num=10, stop=10, pause=2)))):
+        if i != j:
+            continue
+        hasil_link.append(inputan[i] + " " + list(search(query, tld="co.in", num=10, stop=10, pause=2))[j])
+print(hasil_link)
