f4e72815137922c8af4194441d9092889a9d48e1
Regi Apriandi <69528812+regiapriandi012@users.noreply.github.com>
2022-11-12 02:08:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,188 +1,44 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for
 from googlesearch import search
-import PyPDF2
-import os
-from werkzeug.utils import secure_filename
-
-path = os.getcwd()
-UPLOAD_FOLDER = os.path.join(path, 'uploads')
-ALLOWED_EXTENSIONS = {'pdf'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.add_url_rule(
-    "/uploads/<name>", endpoint="download_file", build_only=True
-)
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 app.config['SECRET_KEY'] = 'super secret key'
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if request.form['text'] != '' and request.files['file'].filename == '':
-            word = request.form['text']
-            masukan = "word"
-            with open('word.txt', 'w', encoding='utf-8') as f:
-                f.write(word)
-            return redirect(url_for('plagiarism', name=masukan) + "#hasil")
-        elif request.files['file'].filename != '' and request.form['text'] == '':
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            file = request.files['file']
-            # If the user does not select a file, the browser submits an
-            # empty file without a filename.
-            if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                return redirect(url_for('plagiarism', name=filename) + "#hasil")
-        else:
-            flash('Please fill the form')
-            return redirect(request.url)
+        word = request.form['text']
+        with open('word.txt', 'w', encoding='utf-8') as f:
+            f.write(word)
+        return redirect(url_for('plagiarism')+"#hasil")
     return render_template("index.html")
 
-
-@app.route('/plagiarism/<name>', methods=['GET', 'POST'])
-def plagiarism(name):
+@app.route('/plagiarism', methods=['GET', 'POST'])
+def plagiarism():
     link_output = []
     hasil_plagiarism = []
     hasil_link = []
     hasil_persen = 0
     inputan_mentah = ""
-    inputan = []
-    filename = ""
-    text = ""
-
     if request.method == 'POST':
-        if request.form['text'] != '' and request.files['file'].filename == '':
-            word = request.form['text']
-            filename += "word"
-            with open('word.txt', 'w', encoding='utf-8') as f:
-                f.write(word)
-        elif request.files['file'].filename != '' and request.form['text'] == '':
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            file = request.files['file']
-            # If the user does not select a file, the browser submits an
-            # empty file without a filename.
-            if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-                pdfFileObj = open('uploads/{}'.format(filename), 'rb')
-                pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-                num_pages = pdfReader.numPages
-                count = 0
-                while count < num_pages:
-                    pageObj = pdfReader.getPage(count)
-                    count += 1
-                    text += pageObj.extractText()
-        else:
-            flash('Please fill the form')
-            return redirect(request.url)
-
-        if filename == "word":
-            if request.method == 'POST':
-                inputan_mentah += request.form['text']
-            else:
-                f = open("word.txt", "r")
-                inputan_mentah += f.read()
-            inputan += inputan_mentah.replace("\n", " ").split(". ")
-            for i in range(len(inputan)):
-                query = '"' + inputan[i].strip().replace(".", "").replace('"', "'") + '"'
-                for j in range(len(list(search(query, tld="com", num=10, stop=10, pause=2)))):
-                    if i != j:
-                        continue
-                    if ('id.linkedin.com' or 'linkedin.com' or 'youtube.com' or 'instagram.com' or 'facebook.com' or 'tokopedia.com' or 'twitter.com' or 'reddit.com' or 'bukalapak.com' or 'shopee.com' or 'blibli.com') in list(search(query, tld="com", num=10, stop=10, pause=2))[j]:
-                        continue
-                    hasil_plagiarism.append(inputan[i])
-                    hasil_link.append(list(search(query, tld="com", num=10, stop=10, pause=2))[j])
-            count = len(inputan)
-            count_hasil = len(hasil_link)
-            hasil_persen += (count_hasil / count) * 100
-            for i in range(len(hasil_link)):
-                link_output.append(hasil_link[i])
-        else:
-            inputan += text.replace("\n", " ").split(". ")
-            for i in range(len(inputan)):
-                query = '"' + inputan[i].strip().replace(".", "").replace('"', "'") + '"'
-                for j in range(len(list(search(query, tld="com", num=10, stop=10, pause=2)))):
-                    if i != j:
-                        continue
-                    if ('id.linkedin.com' or 'linkedin.com' or 'youtube.com' or 'instagram.com' or 'facebook.com' or 'tokopedia.com' or 'twitter.com' or 'reddit.com' or 'bukalapak.com' or 'shopee.com' or 'blibli.com') in list(search(query, tld="com", num=10, stop=10, pause=2))[j]:
-                        continue
-                    hasil_plagiarism.append(inputan[i])
-                    hasil_link.append(list(search(query, tld="com", num=10, stop=10, pause=2))[j])
-            count = len(inputan)
-            count_hasil = len(hasil_link)
-            hasil_persen += (count_hasil / count) * 100
-            for i in range(len(hasil_link)):
-                link_output.append(hasil_link[i])
-
+        inputan_mentah += request.form['text']
     else:
-        if name == "word":
-            if request.method == 'POST':
-                inputan_mentah += request.form['text']
-            else:
-                f = open("word.txt", "r")
-                inputan_mentah += f.read()
-            inputan += inputan_mentah.replace("\n", " ").split(". ")
-            for i in range(len(inputan)):
-                query = '"' + inputan[i].strip().replace(".", "").replace('"', "'") + '"'
-                for j in range(len(list(search(query, tld="com", num=10, stop=10, pause=2)))):
-                    if i != j:
-                        continue
-                    if ('id.linkedin.com' or 'linkedin.com' or 'youtube.com' or 'instagram.com' or 'facebook.com' or 'tokopedia.com' or 'twitter.com' or 'reddit.com' or 'bukalapak.com' or 'shopee.com' or 'blibli.com') in list(search(query, tld="com", num=10, stop=10, pause=2))[j]:
-                        continue
-                    hasil_plagiarism.append(inputan[i])
-                    hasil_link.append(list(search(query, tld="com", num=10, stop=10, pause=2))[j])
-            count = len(inputan)
-            count_hasil = len(hasil_link)
-            hasil_persen += (count_hasil / count) * 100
-            for i in range(len(hasil_link)):
-                link_output.append(hasil_link[i])
-        else:
-            pdfFileObj = open('uploads/{}'.format(name), 'rb')
-            pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-            num_pages = pdfReader.numPages
-            count = 0
-            text = ""
-            while count < num_pages:
-                pageObj = pdfReader.getPage(count)
-                count += 1
-                text += pageObj.extractText()
-            inputan += text.replace("\n", " ").split(". ")
-            for i in range(len(inputan)):
-                query = '"' + inputan[i].strip().replace(".", "").replace('"', "'") + '"'
-                for j in range(len(list(search(query, tld="com", num=10, stop=10, pause=2)))):
-                    if i != j:
-                        continue
-                    if ('id.linkedin.com' or 'linkedin.com' or 'youtube.com' or 'instagram.com' or 'facebook.com' or 'tokopedia.com' or 'twitter.com' or 'reddit.com' or 'bukalapak.com' or 'shopee.com' or 'blibli.com') in list(search(query, tld="com", num=10, stop=10, pause=2))[j]:
-                        continue
-                    hasil_plagiarism.append(inputan[i])
-                    hasil_link.append(list(search(query, tld="com", num=10, stop=10, pause=2))[j])
-            count = len(inputan)
-            count_hasil = len(hasil_link)
-            hasil_persen += (count_hasil / count) * 100
-            for i in range(len(hasil_link)):
-                link_output.append(hasil_link[i])
-
-    return render_template("index.html", hasil_persen=hasil_persen, data=inputan, hasil_plagiarism=hasil_plagiarism,
-                           link_output=link_output, hasil_link=hasil_link)
-
+        f = open("word.txt", "r")
+        inputan_mentah += f.read()
+    inputan = inputan_mentah.replace("\n", " ").split(". ")
+    for i in range(len(inputan)):
+        query = '"' + inputan[i].strip().replace(".", "").replace('"', "'") + '"'
+        for j in range(len(list(search(query, tld="com", num=10, stop=10, pause=2)))):
+            if i != j:
+                continue
+            hasil_plagiarism.append(inputan[i])
+            hasil_link.append(list(search(query, tld="com", num=10, stop=10, pause=2))[j])
+    count = len(inputan)
+    count_hasil = len(hasil_link)
+    hasil_persen += (count_hasil / count) * 100
+    for i in range(len(hasil_link)):
+        link_output.append(hasil_link[i])
+    return render_template("index.html", hasil_persen=hasil_persen, data=inputan, hasil_plagiarism=hasil_plagiarism, link_output=link_output, hasil_link=hasil_link)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=False)
